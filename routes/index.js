84288407c100d60328205d7e02fde5ed07dda877
henry.sweat <hi@henrysweat.com>
2022-05-10 12:38:37 -0400

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Coastal Water Forecast from Melbourne Weather Station' });
+});
+
+module.exports = router;
